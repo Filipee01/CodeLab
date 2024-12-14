d32bd8b02cec616d74e02be900063babab278b45
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-12-14 18:00:44 -0300

    change font-size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ const Header = () => {
   return (
     <div className="flex flex-col gap-8 w-[450px] h-[208px] bg-dark_20">
       <div className="flex justify-center items-center text-center">
-        <h2 className="mt-12 text-dark_60 font-medium">
+        <h2 className="mt-12 text-dark_60 font-medium text-[24px]">
           <span className="text-brand_color">Code</span>Lab
         </h2>
       </div>
       <div>
         <input
-          className="w-[327px] mx-6 h-[49px] p-4 font-serif text-sm text-dark_50 bg-dark_30 border-2 border-dark_40 rounded-md"
+          className="w-[327px] mx-16 h-[49px] p-4 font-serif text-sm text-dark_50 bg-dark_30 border-2 border-dark_40 rounded-md"
           type="text"
           placeholder="Pesquisar no blog"
         />
